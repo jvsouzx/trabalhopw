95309923aca559b31a9e08d42387aeb0a57cbda9
Jorge <goncalves2917@gmail.com>
2020-12-01 14:26:33 -0300

    adding js

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+
+    $('#img1').on( "click", function() {
+        $(this).animate({
+        width:'70%',
+        height:'70%'
+        },1000);
+        });
+
+    $('#img1').mouseout(function() {
+        $(this).animate({
+        width:'50%',
+        height:'50%'
+        },1000);
+        });
+
+});
+
+$(document).ready(function(){
+
+    $('#img2').on( "click", function() {
+        $(this).animate({
+        width:'70%',
+        height:'70%'
+        },1000);
+        });
+
+    $('#img2').mouseout(function() {
+        $(this).animate({
+        width:'50%',
+        height:'50%'
+        },1000);
+        });
+
+});
+
+$(document).ready(function(){
+
+    $('#img3').on( "click", function() {
+        $(this).animate({
+        width:'70%',
+        height:'70%'
+        },1000);
+        });
+
+    $('#img3').mouseout(function() {
+        $(this).animate({
+        width:'50%',
+        height:'50%'
+        },1000);
+        });
+
+});
+
+$(document).ready(function(){
+
+    $('#img4').on( "click", function() {
+        $(this).animate({
+        width:'70%',
+        height:'70%'
+        },1000);
+        });
+
+    $('#img4').mouseout(function() {
+        $(this).animate({
+        width:'50%',
+        height:'50%'
+        },1000);
+        });
+
+});
